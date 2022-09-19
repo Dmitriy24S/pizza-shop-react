6c876c4bc6b2b4f4842b7055700f6bea313854f5
Dmitriy24S <devilins24@gmail.com>
2022-09-19 16:42:34 +0300

    feat: add clear search input svg icon, position/spacing/style, wrap search icon in label for input

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { ReactComponent as CloseIcon } from "../../assets/img/close-icon.svg";
 import { ReactComponent as SearchIcon } from "../../assets/img/search-icon.svg";
 import styles from "./Search.module.scss";
 
 const Search = () => {
   return (
     <div className={styles.search}>
-      <SearchIcon />
-      <input type="text" placeholder="Search pizza" />
+      <label htmlFor="search-input" className={styles.searchIcon}>
+        <SearchIcon />
+      </label>
+      <input type="text" id="search-input" placeholder="Search pizza" />
+      <button aria-label="clear input" className={styles.closeIcon}>
+        <CloseIcon />
+      </button>
     </div>
   );
 };
