c830e10a0d35f4d5e5b50be3006bb8aa2a2c4b6a
Dmitriy24S <devilins24@gmail.com>
2022-10-03 10:47:25 +0300

    refactor: code split: switch to lazy routing with React.lazy & react-loadable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { Suspense } from "react";
+import Loadable from "react-loadable";
 import { createBrowserRouter, Outlet, Route, RouterProvider, Routes } from "react-router-dom";
 
 import Header from "./components/Header/Header";
-import Cart from "./Pages/Cart";
-import ErrorPage from "./Pages/ErrorPage/ErrorPage";
-import Home from "./Pages/Home";
-import PizzaSinglePage from "./Pages/PizzaSinglePage/PizzaSinglePage";
+// import Home from "./Pages/Home";
+// import Cart from "./Pages/Cart";
+// import PizzaSinglePage from "./Pages/PizzaSinglePage/PizzaSinglePage";
+// import ErrorPage from "./Pages/ErrorPage/ErrorPage";
 import "./scss/app.scss";
+
+// browser side - react lazy
+const Home = React.lazy(() => import(/* webpackChunkName: 'Home' */ "./Pages/Home"));
+const PizzaSinglePage = React.lazy(
+  () => import(/* webpackChunkName: 'PizzaSinglePage' */ "./Pages/PizzaSinglePage/PizzaSinglePage")
+);
+const ErrorPage = React.lazy(
+  () => import(/* webpackChunkName: 'ErrorPage' */ "./Pages/ErrorPage/ErrorPage")
+);
+// const Cart = React.lazy(() => import(/* webpackChunkName: 'Cart' */ "./Pages/Cart"));
+
+// server side rendering - react loadable
+const Cart = Loadable({
+  loader: () => import(/* webpackChunkName: 'Cart' */ "./Pages/Cart"),
+  loading: () => <h1>Loading your cart</h1>,
+});
 
 function App() {
   return (
@@ -15,10 +32,38 @@ function App() {
       <div className="container">
         {/* <Outlet /> // New React Router option - show children components */}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="pizza/:id" element={<PizzaSinglePage />} />
-          <Route path="*" element={<ErrorPage />} />
+          <Route
+            path="/"
+            element={
+              <Suspense>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="cart"
+            element={
+              <Suspense>
+                <Cart />
+              </Suspense>
+            }
+          />
+          <Route
+            path="pizza/:id"
+            element={
+              <Suspense fallback={<h1>Loading pizza</h1>}>
+                <PizzaSinglePage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <Suspense>
+                <ErrorPage />
+              </Suspense>
+            }
+          />
         </Routes>
       </div>
     </div>
